ff20f0f28f23f67e5958a97f2108e605231d87a0
shenshuu <michaelshen85@gmail.com>
2023-11-27 16:04:15 -0500

    Add features 1 and 2 for hw6

--- Part_C/wk6/hw6assignment.rb
+++ Part_C/wk6/hw6assignment.rb
@@ -5,18 +5,79 @@
 
 class MyPiece < Piece
     # The constant All_My_Pieces should be declared here
+    All_My_Pieces = [
+      # [[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+      # rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+      # [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+      # [[0, 0], [0, -1], [0, 1], [0, 2]]],
+      # rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+      # rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+      # rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+      # rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z
+      rotations([[0,0],[0,1],[1,1]]), # corner
+      rotations([[0,0],[1,0],[0,1],[1,1],[2,1]]), # 2x3
+      [[[-2, 0], [-1, 0], [0, 0], [1, 0], [2,0]], # 1x5
+      [[0, -2], [0, -1], [0, 0], [0, 1], [0, 2]]],
+    ]
   
     # your enhancements here
-  
+    def self.next_piece (board)
+      MyPiece.new(All_My_Pieces.sample, board)
+    end
+
+    def area
+      current_rotation.length
+    end
+
   end
   
   class MyBoard < Board
     # your enhancements here
+
+    def initialize (game)
+      super
+      @current_block = MyPiece.next_piece(self)
+      @block_area = @current_block.area
+    end
+
+    def next_piece
+      @current_block = MyPiece.next_piece(self)
+      @block_area = @current_block.area
+      @current_pos = nil
+    end
+
+    def store_current
+      locations = @current_block.current_rotation
+      displacement = @current_block.position
+      (0...@block_area).each{|index| 
+        current = locations[index];
+        @grid[current[1]+displacement[1]][current[0]+displacement[0]] = 
+        @current_pos[index]
+      }
+      remove_filled
+      @delay = [@delay - 2, 80].max
+    end
   
   end
   
   class MyTetris < Tetris
     # your enhancements here
+    def key_bindings
+      super
+      @root.bind('u', proc {
+        @board.rotate_clockwise
+        @board.rotate_clockwise
+      })
+    end
+
+    def set_board
+      @canvas = TetrisCanvas.new
+      @board = MyBoard.new(self)
+      @canvas.place(@board.block_size * @board.num_rows + 3,
+                    @board.block_size * @board.num_columns + 6, 24, 80)
+      @board.draw
+    end
+
   
   end
   
