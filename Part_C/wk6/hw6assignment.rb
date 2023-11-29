758a17af0ca8116de2631cc924ed1a66bc744cc5
shenshuu <michaelshen85@gmail.com>
2023-11-29 13:15:13 -0500

    Add cheat piece

--- Part_C/wk6/hw6assignment.rb
+++ Part_C/wk6/hw6assignment.rb
@@ -6,16 +6,16 @@
 class MyPiece < Piece
     # The constant All_My_Pieces should be declared here
     All_My_Pieces = [
-      # [[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
-      # rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
-      # [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
-      # [[0, 0], [0, -1], [0, 1], [0, 2]]],
-      # rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
-      # rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
-      # rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
-      # rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z
-      rotations([[0,0],[0,1],[1,1]]), # corner
-      rotations([[0,0],[1,0],[0,1],[1,1],[2,1]]), # 2x3
+      [[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+      rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+      [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+      [[0, 0], [0, -1], [0, 1], [0, 2]]],
+      rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+      rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+      rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+      rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z
+      rotations([[0, 0], [0, 1], [1, 1]]), # corner
+      rotations([[0, 0], [1, 0], [0, 1], [1, 1], [2, 1]]), # 2x3
       [[[-2, 0], [-1, 0], [0, 0], [1, 0], [2,0]], # 1x5
       [[0, -2], [0, -1], [0, 0], [0, 1], [0, 2]]],
     ]
@@ -23,6 +23,10 @@ class MyPiece < Piece
     # your enhancements here
     def self.next_piece (board)
       MyPiece.new(All_My_Pieces.sample, board)
+    end
+
+    def self.cheat_piece (board)
+      MyPiece.new([[0,0]], board)
     end
 
     def area
@@ -33,16 +37,25 @@ class MyPiece < Piece
   
   class MyBoard < Board
     # your enhancements here
+    attr_accessor :score, :cheat_pieces
 
     def initialize (game)
       super
       @current_block = MyPiece.next_piece(self)
       @block_area = @current_block.area
+      @cheat_pieces = 0
+      @score = 1000
     end
 
     def next_piece
       @current_block = MyPiece.next_piece(self)
       @block_area = @current_block.area
+      @current_pos = nil
+    end
+
+    def cheat_piece
+      @current_block = MyPiece.cheat_piece(self)
+      @block_area = 1
       @current_pos = nil
     end
 
@@ -57,6 +70,46 @@ class MyPiece < Piece
       remove_filled
       @delay = [@delay - 2, 80].max
     end
+
+    def drop_all_the_way
+      if @game.is_running?
+        ran = @current_block.drop_by_one
+        @current_pos.each{|block| block.remove}
+        while ran
+          @score += 1
+          ran = @current_block.drop_by_one
+        end
+        draw
+        store_current
+        if !game_over?
+          if cheat_pieces > 0
+            @cheat_pieces -= 1
+            cheat_piece
+          else
+            next_piece
+          end
+        end
+        @game.update_score
+        draw
+      end
+    end
+
+    def run
+      ran = @current_block.drop_by_one
+      if !ran
+        store_current
+        if !game_over?
+          if cheat_pieces > 0
+            @cheat_pieces -= 1
+            cheat_piece
+          else
+            next_piece
+          end
+        end
+      end
+      @game.update_score
+      draw
+    end
   
   end
   
@@ -67,6 +120,12 @@ class MyPiece < Piece
       @root.bind('u', proc {
         @board.rotate_clockwise
         @board.rotate_clockwise
+      })
+      @root.bind('c', proc {
+        if @board.score >= 100
+          @board.cheat_pieces += 1
+          @board.score -= 100
+        end
       })
     end
 
